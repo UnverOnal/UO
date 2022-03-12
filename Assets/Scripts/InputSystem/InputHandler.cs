8a0a15366d4eb542739659d3110e0b18f740a410
UnverOnal <onalunver@gmail.com>
2022-03-12 21:32:13 +0300

    Create basic input system

---
+++ Assets/Scripts/InputSystem/InputHandler.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace UO.InputSystem
+{
+    public static class InputHandler
+    {
+        
+    }
+    
+}
+
