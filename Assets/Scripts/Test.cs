ae23f92dccf34bd68b668b82a5bd1083f17577b4
UnverOnal <onalunver@gmail.com>
2022-03-21 21:02:00 +0300

    Delete input system

--- Assets/Scripts/Test.cs
+++ Assets/Scripts/Test.cs
@@ -2,21 +2,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UO.Extensions;
-using UO.InputSystem;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
 
 public class Test : MonoBehaviour
 {
-    
 
-    void Start()
-    {
-    }
-
-    void Update()
-    {
-        InputData.Update();
-
-        Vector3 targetPosition = this.transform.position + Vector3.up * InputData.input.y * 100f;
-        this.transform.position = Vector3.Lerp(this.transform.position, targetPosition, Time.deltaTime);
-    }
 }
